5d6b2653344c00bf7459f8a650a5b1198a4754b1
Isterangel <istartraore@gmail.com>
2024-09-25 00:53:29 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2347063956321'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '22373633513'  //creator number
+global.ownername = 'I̥ͦ𓊈𒆜𝕴𝖘𝖙𝖊𝖗𒆜𓊉I̥ͦ『𝐋𝐆𝐓』『༎ismael༎』' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
